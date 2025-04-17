a0f555e00f77f8402b5d251d042f9a135e0aac55
ikernm <ikernieblamoreno@hotmail.com>
2025-04-17 18:30:07 +0200

    error en elementoMensaje

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,17 @@ import "./style.css";
 
 const generarNumeroAleatorio = (): number => Math.floor(Math.random()* 6) + 1;
 
+const botonTirarDado = document.getElementById("tirar-dado");
+const botonPlantar = document.getElementById("me-planto");
+
+const imagenDado = document.getElementById("dado");
+const mensajeDado = document.getElementById("mensaje-dado");
+const puntuacionDado = document.getElementById("puntuacion-dado");
+
+    // Le asignamos el valor máximo de puntos y el valor inicial de puntos
+    const MAXIMO_PUNTOS = 50;
+    let puntos = 0;
+
 // Definimos los estados posibles del juego
 type Estado =
  | "NO_ES_EL_NUMERO_6"
@@ -9,23 +20,14 @@ type Estado =
  | "MAXIMO_PUNTOS"
  | "ME_PLANTO";
 
- // Le asignamos el valor máximo de puntos y el valor inicial de puntos
- const MAXIMO_PUNTOS = 50;
- let puntos = 0;
+const elementoMensaje = (texto: string, estado: Estado) => {
 
-// Al pulsar el botón ¡Tira el dado!, nos devuelve la imagen y los textos de la puntuación y el mensaje
-const botonTirarDado = document.getElementById("tirar-dado");
-const imagenDado = document.getElementById("dado");
-const puntosDado = document.getElementById("puntuacion-dado");
-
-
-if (botonTirarDado && botonTirarDado instanceof HTMLButtonElement &&
-    imagenDado && imagenDado instanceof HTMLImageElement &&
-    puntosDado && puntosDado instanceof HTMLDivElement
-    ) {
-
-        botonTirarDado.addEventListener("click", () => {
-
+    if(mensajeDado && mensajeDado instanceof HTMLDivElement &&
+        puntuacionDado && puntuacionDado instanceof HTMLDivElement &&
+        imagenDado && imagenDado instanceof HTMLImageElement) {
+     
+        let mensaje: string = texto;
+        
         // Generamos un número aleatorio entre 1 y 6
         const numeroAleatorio = generarNumeroAleatorio();
 
@@ -34,53 +36,61 @@ if (botonTirarDado && botonTirarDado instanceof HTMLButtonElement &&
         imagenDado.alt = `Cara ${numeroAleatorio}`;
 
         // Cambiamos el texto de la puntuación
-        puntosDado.innerHTML = `Puntuación: ${puntos}`;
+        puntuacionDado.innerHTML = `Puntuación: ${puntos}`;
+        mensajeDado.innerHTML = mensaje;
 
-        // Cambiamos el mensaje del dado según el número aleatorio
-        const mensajePuntos = (texto: string, estado: Estado) => {
-            if(mensajePuntos && mensajePuntos instanceof HTMLDivElement) {
-                   // NO SE CÓMO CONTINUAR CON EL CÓDIGO SIN QUE ME DE ERROR  
-                let mensaje: string = texto;
+        switch (estado) {
 
-            switch (estado) {
-                case "NO_ES_EL_NUMERO_6":
-                    mensaje =`Has sacado un ${numeroAleatorio}, has obtenido 10 puntos. Sigue tirando para obtener más de 50 puntos`;
-                    puntos + 10;
-                    break;
-                case "ES_EL_NUMERO_6":
-                    mensaje = `Has sacado un ${numeroAleatorio}, has perdido`;
-                    break;
-                case "MAXIMO_PUNTOS":
-                    if(puntos >= MAXIMO_PUNTOS) {
-                        mensaje = `Tus puntos actuales son ${puntos}, que son mayores a 50, por tanto, has ganado`;}
-                    break;
-                case "ME_PLANTO":
-                    mensaje = `Te has plantado con ${puntos} puntos`;
-                    break;
-                default:
-                    mensaje = `Error: ${estado}`;
-                    break;
+            case "NO_ES_EL_NUMERO_6":
+                if(numeroAleatorio !== 6) {
+                mensaje =`Has sacado un ${numeroAleatorio}, has obtenido 10 puntos. Sigue tirando para obtener más de 50 puntos`;
+                puntos += 10;
                 }
+                break;
 
-                const elementoMensajePuntos = document.getElementById("mensaje-puntos");
-                if (elementoMensajePuntos && elementoMensajePuntos instanceof HTMLDivElement) {
-                    elementoMensajePuntos.innerHTML = mensaje;
-                } else {
-                    console.error("Elemento mensaje puntos no encontrado o no es un HTMLDivElement");
+            case "ES_EL_NUMERO_6":
+                if(numeroAleatorio == 6) {
+                mensaje = `Has sacado un ${numeroAleatorio}, has perdido todos los puntos`;
+                puntos = 0;
                 }
-            }
-        }
-        
-        // Al pulsar el botón ¡Me planto!, nos devuelve la imagen y los textos de la puntuación y el mensaje
-        const botonMePlanto = document.getElementById("me-planto");
-        if (botonMePlanto && botonMePlanto instanceof HTMLButtonElement) {
+                break;
 
-            botonMePlanto.addEventListener("click", () => {
-             mensajePuntos("Te has plantado", "ME_PLANTO");    
-            botonMePlanto.disabled = true;
-            })
+            case "MAXIMO_PUNTOS":
+                if(puntos >= MAXIMO_PUNTOS) {
+                mensaje = `Tus puntos actuales son ${puntos}, que son mayores a 50, por tanto, has ganado`;
+                }
+                break;
+
+            case "ME_PLANTO":
+                mensaje = `Te has plantado con ${puntos} puntos`;
+                break;
+
+            default:
+                mensaje = `Error: ${estado}`;
+                break;
         }
-     })
+    }
 }
 
-// Actualizar el DOM con el número de puntos y el mensaje??
+// Llamamos a la función de hacerle click al botón de ¡Tira el dado!
+
+if (botonTirarDado && botonTirarDado instanceof HTMLButtonElement) {
+    botonTirarDado.addEventListener("click", () => {
+        generarNumeroAleatorio();
+        elementoMensaje(texto,estado);
+    });
+}
+
+// Llamamos a la función de hacerle click al botón de ¡Me planto!
+
+if(botonPlantar && botonPlantar instanceof HTMLButtonElement &&
+    botonTirarDado && botonTirarDado instanceof HTMLButtonElement) {
+
+    botonPlantar.addEventListener("click", () => {
+        elementoMensaje(mensaje,"ME_PLANTO");
+        botonPlantar.disabled = true;
+        botonTirarDado.disabled = true;
+    });
+}
+
+
